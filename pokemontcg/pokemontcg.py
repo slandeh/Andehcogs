1eaa45257858228a6c3337c7d546e4b03bd85c7a
slandeh <34135797+slandeh@users.noreply.github.com>
2019-09-04 13:37:10 -0700

    Fix new lines

--- pokemontcg/pokemontcg.py
+++ pokemontcg/pokemontcg.py
@@ -89,12 +89,12 @@ def search(name):
     # Fix for entering one letter to search for a card
     if len(name) == 1 and name == "N":
         return ("Matches for search '%s'\n" % name
-                "N - Noble Victories 92/101 (`bw3-92`)\n"
-                "N - Noble Victories 101/101 (`bw3-101`)\n"
-                "N - Dark Explorers 96/108 (`bw5-96`)\n"
-                "N - BW Black Star Promo 100 (`bwp-100`)\n"
-                "N - Fates Collide 105/124 (`xy10-105`)\n"
-                "N - Fates Collide 105a/124 (`xy10-105a`)")
+                "N - Noble Victories 92/101 (`bw3-92`)\n
+                N - Noble Victories 101/101 (`bw3-101`)\n
+                N - Dark Explorers 96/108 (`bw5-96`)\n
+                N - BW Black Star Promo 100 (`bwp-100`)\n
+                N - Fates Collide 105/124 (`xy10-105`)\n
+                N - Fates Collide 105a/124 (`xy10-105a`)")
     elif len(name) == 1:
         return ("Only one letter was entered! The list will take too long!")
 

2a95542017a6f80801d4bdb15494875ee9fe219d
Andeh <34135797+slandeh@users.noreply.github.com>
2021-06-22 15:51:59 -0700

    Update pokemontcgv2.py
    Fix to search function

--- pokemontcgv2/pokemontcgv2.py
+++ pokemontcgv2/pokemontcgv2.py
@@ -105,29 +105,29 @@ def search(name):
     # If there is exactly one match, save time for the user and give the
     # !show output instead
     if len(cards) == 1:
-        return (show(cards[0].name, cards[0].set_code), 1)
+        return (show(cards[0].name, cards[0].set.id), 1)
 
     # If there are matches, build a string with the name, set and set code
     # of every match
     cards_with_sets = []
     for card in cards:
-        card_set = Set.find(card.set_code)
+        card_set = Set.find(card.set.id)
 
         # Re-arrange the release data so it is ISO
-        date_split = card_set.release_date.split("/")
-        card_set.release_date = "%s-%s-%s" % (date_split[2], date_split[0], date_split[1])
+        date_split = card_set.releaseDate.split("/")
+        card_set.releaseDate = "%s-%s-%s" % (date_split[2], date_split[0], date_split[1])
 
         cards_with_sets.append((card, card_set))
 
     # Sort the list of cards by set release date
-    cards_with_sets.sort(key = lambda card : card[1].release_date)
+    cards_with_sets.sort(key = lambda card : card[1].releaseDate)
 
     # Create the returned string
     return_str = "Matches for search '%s'\n" % name
     for card in cards_with_sets:
-        return_str += ("%s - %s %s/%s (`%s-%s`)\n" % (card[0].name, card[1].name,
-                                                      card[0].number, card[1].total,
-                                                      card[1].code, card[0].number))
+        return_str += ("%s - %s %s/%s (`%s-%s`)\n" % (card.name, card.set.name,
+                                                      card.number, card.totalPrinted,
+                                                      card.set.id, card.number))
 
     return (return_str, len(cards_with_sets))
 

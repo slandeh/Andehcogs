7963a0f28222cc96ac148ba831ce8b8844b00ddb
Andeh <34135797+slandeh@users.noreply.github.com>
2022-01-21 15:26:59 -0800

    Create compendium.py

---
+++ Compendium/compendium.py
@@ -0,0 +1,112 @@
+import requests
+import json
+import discord
+from concurrent.futures import ThreadPoolExecutor
+from functools import lru_cache
+from redbot.core import commands
+from redbot.core.utils.chat_formatting import pagify
+
+# Maximum number of rulings before giving up on creating an embed.
+MAX_RULINGS = 3
+
+# Variables for making Compendium Requests
+url = "https://compendium.pokegym.net/wp-json/relevanssi/v1/search?keyword="
+ruletype = "&type=ruling"
+headers = {"Authorization": "Basic Um90b21QaG9uZTpYc2xOIHBXa1YgTFVnciAxdHNGIHBkM08gTHNvMw==", "User-Agent": "PostmanRuntime/7.28.4"}
+payload = {}
+
+# Given string "searchtext", search the compendium for rulings.
+def compsearch(text):
+    embed = None
+    if text == "":
+        return ("", 0)
+    
+    # Convert strings to appropriate url strings
+    if " " in text:
+        finaltext = text.replace(" ", "+")
+    else:
+        finaltext = text
+
+    # Create the request URL
+    finalurl = url + finaltext + ruletype
+
+    response = requests.get(finalurl, headers=headers)
+
+    r = json.loads(response.text)
+
+    # Validate response is not empty
+    if r["data"] is None:
+        return ("", 0)
+    elif len(r) > MAX_RULINGS:
+        return ("", len(r))
+
+    # Set some embed variabled
+    title = text.title()
+    compendium_ico = 'https://compendium.pokegym.net/wp-content/uploads/2021/08/cropped-cpdm_ball-32x32.png'
+    
+    # Let's create a Discord Embed!
+    if len(r) == 1:
+        url = r[0]['link']
+        question = r[0]['meta']['question']
+        answer = r[0]['meta']['ruling']
+        source = r[0]['meta']['source']
+
+        embed = discord.Embed(title=title, url=url)
+
+        embed.add_field(name="Question", value=question, inline=True)
+        embed.add_field(name="Answer", value=answer, inline=True)
+
+        embed.set_footer(text=source, icon_url=compendium_ico)
+
+        return (embed, len(r))
+    
+    elif len(r) > 1:
+        url = finalurl
+
+        for rule in r:
+            question = r[rule]['meta']['question']
+            answer = r[rule]['meta']['answer'] + ' (' + r[rule]['meta']['source'][0] + ')'
+
+            embed.add_field(name="Question", value=question, inline=True)
+            embed.add_field(name="Answer", value=answer, inline=True)
+
+        embed.set_footer(text="Compendium Team", icon_url=compendium_ico)
+
+        return (embed, len(r))
+
+
+
+class Compendium(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.executor = ThreadPoolExecutor()
+    
+    def __unload(self):
+        self.executor.shutdown(wait=True)
+    
+    async def _run_in_thread(self, *args):
+        return await self.bot.loop.run_in_executor(self.executor, *args)
+
+    async def _smart_send(self, destination, message):
+        if isinstance(message, discord.Embed):
+            await destination.send(embed=message)
+        elif message:
+            for page in pagify(message):
+                await destination.send(page)
+
+    @commands.command(pass_context=True)
+    async def compendium(self, ctx, *, card_name: str):
+        """
+        Returns rulings related to the search terms.
+        Usage:
+            !compendium Abyssal Gate
+            !compendium Recycle Energy Spectral Breach
+        """
+        (message, results) = await self._run_in_thread(compsearch, searchtext)
+
+        if results > MAX_RULINGS:
+            await self._smart_send(ctx.message.channel, "Results list is too long, try using more terms")
+        elif results == 0:
+            await self._smart_send(ctx.message.channel, "No results were found, check the terms you're searching.")
+        else:
+            await self._smart_send(ctx.message.channel, message)

c8a7aa6a060299ffbc34941ad990b1dd78934be4
Andeh <34135797+slandeh@users.noreply.github.com>
2024-03-14 10:36:41 -0700

    Update leveler.py
    fix avatar_url_as => avatar.replace

--- Leveler2/leveler.py
+++ Leveler2/leveler.py
@@ -100,7 +100,7 @@ class Leveler(commands.Cog):
     async def get_avatar(self, user):
         try:
             res = BytesIO()
-            await user.avatar_url_as(format="png", size=1024).save(res, seek_begin=True)
+            await user.avatar.replace(format="png", size=1024).save(res, seek_begin=True)
             return res
         except:
             async with self._session.get(user.avatar_url_as(format="png", size=1024)) as r:
